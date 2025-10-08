1c2a7e81645617d8260029342cda1ab1dd3b4113
Leonard Senanu <senanuld@mail.uc.edu>
2025-10-07 21:33:13 -0400

    Lab07_Task2

---
+++ src/Task2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("loop 2:Count down by 1 from 0 to 30 ");
+        for (int i = 0; i >= 30; i--) {
+            System.out.println(i);
+        }
+        System.out.println("\n");
+    }
+
+}
